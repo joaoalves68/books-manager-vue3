33d838b19ebf104914ba6c2e4c0b36b275ffd1df
João Alves <joao-alvespa@hotmail.com>
2025-02-04 19:58:22 -0300

    feat: authors model

---
+++ app/Models/Authors.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Authors extends Model
+{
+  use HasFactory;
+
+  protected $table = 'authors';
+
+  protected $fillable = [
+    'name',
+    'state',
+  ];
+
+  public function books()
+  {
+    return $this->hasMany(Books::class);
+  }
+}
