0c8b9225e4eb288b5d7ace19d1025d249fe31db4
João Alves <joao-alvespa@hotmail.com>
2025-02-05 17:50:26 -0300

    feat: routes to authors api

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\BooksController;
+use App\Http\Controllers\Api\AuthorsController;
 
 Route::get('/user', function (Request $request) {
   return $request->user();
 })->middleware('auth:sanctum');
 
 Route::resource('/books', BooksController::class);
+Route::resource('/authors', AuthorsController::class);
