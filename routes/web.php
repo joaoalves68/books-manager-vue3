fb7e87d54bc513eea5bacc84c47655983b2df450
João Alves <joao-alvespa@hotmail.com>
2025-02-05 18:43:30 -0300

    feat: web route and controllers for authors

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Web\BooksController;
+use App\Http\Controllers\Web\AuthorsController;
 
 Route::resource('/books', BooksController::class);
+Route::resource('/authors', AuthorsController::class);
