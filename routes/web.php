a13a2b8a561e487994387ef2f14d4c252ef7cec2
João Alves <joao-alvespa@hotmail.com>
2025-02-06 22:15:31 -0300

    feat: routes to login and register, configured middleware auth

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,20 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Web\BooksController;
 use App\Http\Controllers\Web\AuthorsController;
+use App\Http\Controllers\Auth\AuthController;
 
-Route::resource('/books', BooksController::class);
-Route::resource('/authors', AuthorsController::class);
+Route::middleware('auth')->group(function () {
+  Route::get('/', function () {
+    return view('dashboard');
+  })->name('dashboard');
+
+  Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+  Route::resource('/authors', AuthorsController::class);
+  Route::resource('/books', BooksController::class);
+});
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
